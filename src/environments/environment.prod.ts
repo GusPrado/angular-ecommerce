72fd4da38e3466228b16b281fdd58ce6da1d02e1
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-04-10 18:56:16 -0300

    Initial deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDP-0ofFGO8w7jrhBXL6E0nsZmFvQidzAY",
+    authDomain: "gushop-8db2b.firebaseapp.com",
+    databaseURL: "https://gushop-8db2b.firebaseio.com",
+    projectId: "gushop-8db2b",
+    storageBucket: "gushop-8db2b.appspot.com",
+    messagingSenderId: "840375419220",
+    appId: "1:840375419220:web:7d4a37b805964987f508c1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDP-0ofFGO8w7jrhBXL6E0nsZmFvQidzAY",
+    authDomain: "gushop-8db2b.firebaseapp.com",
+    databaseURL: "https://gushop-8db2b.firebaseio.com",
+    projectId: "gushop-8db2b",
+    storageBucket: "gushop-8db2b.appspot.com",
+    messagingSenderId: "840375419220",
+    appId: "1:840375419220:web:7d4a37b805964987f508c1"
+  }
 };
 
 /*
